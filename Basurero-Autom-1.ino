680430dfc2ee59156fe2ea3a5b8782931a5f35ee
camilaVera02 <camilaariadnavera@gmail.com>
2023-02-24 21:34:13 -0300

    arduinocommit

---
+++ Basurero-Autom-1.ino
@@ -0,0 +1,277 @@
+const int AIA = 3;  //motor A
+
+const int AIB = 11;
+
+const int ledTest = 13;
+
+
+
+int bDer = 9;
+int rDer = 5;
+int rIzq = 6;
+int bIzq = 10;
+
+//Inicio variables temporales
+int disparo = 4;
+int echo = 2;
+long tiempoPulso;
+float dstMedida;
+
+void setup() {
+
+Serial.begin(9600);
+  pinMode(bDer, OUTPUT);
+  pinMode(bIzq, OUTPUT);
+  pinMode(rDer, OUTPUT);
+  pinMode(rIzq, OUTPUT);
+
+  pinMode(disparo, OUTPUT);
+  pinMode(echo, INPUT);
+
+
+  pinMode(AIA, OUTPUT);
+  pinMode(AIB, OUTPUT);
+pinMode(ledTest, OUTPUT);
+
+
+
+  digitalWrite(AIA, LOW);
+  digitalWrite(AIB, LOW);
+
+  digitalWrite(ledTest, LOW);
+  digitalWrite(bDer, LOW);
+  digitalWrite(bIzq, LOW);
+  digitalWrite(rDer, LOW);
+  digitalWrite(rIzq, LOW);
+
+  digitalWrite(disparo, LOW);
+
+
+
+  digitalWrite(ledTest, HIGH);
+  delay(500);
+  digitalWrite(ledTest, LOW);
+  delay(500);
+  digitalWrite(ledTest, HIGH);
+  delay(500);
+  digitalWrite(ledTest, LOW);
+  delay(500);
+  digitalWrite(ledTest, HIGH);
+  delay(500);
+  digitalWrite(ledTest, LOW);
+  delay(500);
+  
+}
+
+
+
+void loop() {
+
+
+  digitalWrite(disparo, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(disparo, LOW);
+
+  tiempoPulso = pulseIn(echo, HIGH);
+  dstMedida = 0.0340 / 2 * tiempoPulso;
+
+  Serial.println("Tiempo / Distancia ");
+
+  Serial.print(tiempoPulso);
+
+  Serial.print(" / ");
+
+  Serial.println(dstMedida);
+
+  delay (50);
+
+
+
+
+  if (dstMedida < 25) {
+    abrir(3);
+    delay(500);
+    line();
+    delay(100);
+    flash();
+    delay(100);
+    cerrar();
+    parar();
+    delay(1500);
+  }
+  else {
+    intermitente();
+    analogWrite(AIA, 0);
+    analogWrite(AIB, 0);
+    delay(10);
+  }
+}
+
+
+
+
+
+
+// ------ Function
+
+void abrir(int veces) {
+  for (int j = 0; j < veces; j++) {
+    analogWrite(AIA, 255);
+    analogWrite(AIB, 0);
+    delay(500);
+  }
+}
+
+void cerrarrr(int veces) {
+  for (int j = 0; j < veces; j++) {
+    analogWrite(AIA, 0);
+    analogWrite(AIB, 255);
+    delay(500);
+  }
+}
+
+
+void parar()
+{
+  analogWrite(AIA, 0);
+  analogWrite(AIB, 0);
+  delay(500);
+}
+
+void cerrar()
+{
+  analogWrite(AIA, 0);
+  analogWrite(AIB, 200);
+  delay(500);
+}
+
+void intermitente()
+{
+  analogWrite(rDer, 100);
+  analogWrite(rIzq, 100);
+  delay(100);
+  analogWrite(rDer, 255);
+  analogWrite(rIzq, 255);
+  delay(100);
+  desv();
+  delay(100);
+  analogWrite(bDer, 100);
+  analogWrite(bIzq, 100);
+  delay(100);
+  analogWrite(bDer, 255);
+  analogWrite(bIzq, 255);
+  delay(100);
+  desb();
+  delay(100);
+}
+
+void desv()
+{
+  analogWrite(rDer, 150);
+  analogWrite(rIzq, 150);
+  delay(100);
+  analogWrite(rDer, 0);
+  analogWrite(rIzq, 0);
+  delay(50);
+}
+
+
+void desb()
+{
+  analogWrite(bDer, 150);
+  analogWrite(bIzq, 150);
+  delay(100);
+  analogWrite(bDer, 0);
+  analogWrite(bIzq, 0);
+  delay(100);
+}
+
+void flash(){
+ analogWrite(bIzq, 255);
+ analogWrite(bDer, 255);
+ delay(300);
+  analogWrite(bIzq, 0);
+ analogWrite(bDer, 0);
+ delay(100);
+
+ analogWrite(rDer, 255);
+ analogWrite(rIzq, 255);
+ delay(100);
+  analogWrite(rIzq, 0);
+ analogWrite(rDer, 0);
+ delay(100);
+}
+
+void line()
+{
+ analogWrite(bIzq, 255);
+ delay(80);
+ analogWrite(bIzq, 0);
+ delay(80);
+ analogWrite(rIzq, 255);
+ delay(80);
+ analogWrite(rIzq, 0);
+ delay(80);
+ analogWrite(rDer, 255);
+ delay(80);
+ analogWrite(rDer, 0);
+ delay(80);
+ analogWrite(bDer, 255);
+ delay(80);
+ analogWrite(bDer, 0);
+ delay(80);
+ analogWrite(rDer, 255);
+ delay(80);
+ analogWrite(rDer, 0);
+ delay(80);
+  analogWrite(rIzq, 255);
+  delay(80);
+ analogWrite(rIzq, 0);
+ delay(80);
+  analogWrite(bIzq, 255);
+  delay(80);
+ analogWrite(bIzq, 0);
+ delay(100);
+}
+
+/*void izquierda()
+  {analogWrite(bIzq, 255);
+ analogWrite(bIzq, 0);
+ delay(50);
+ analogWrite(rIzq, 255);
+ analogWrite(rIzq, 0);
+ delay(50);
+ analogWrite(rDer, 255);
+ analogWrite(rDer, 0);
+ delay(50);
+ analogWrite(bDer, 255);
+ analogWrite(bDer, 0);
+ delay(50);
+ analogWrite(rDer, 255);
+ analogWrite(rDer, 0);
+ delay(50);
+  analogWrite(rIzq, 255);
+ analogWrite(rIzq, 0);
+ delay(50);
+  analogWrite(bIzq, 255);
+ analogWrite(bIzq, 0);
+ delay(1000);
+  analogWrite(AIA, 0);
+  analogWrite(AIB, 255);
+  analogWrite(BIA, 255);
+  analogWrite(BIB, 0);
+  delay(500);
+  }
+
+
+  void derecha()
+  {
+  analogWrite(AIA, 255);
+  analogWrite(AIB, 0);
+  analogWrite(BIA, 0);
+  analogWrite(BIB, 255);
+  delay(500);
+  }
+
+
+  }*/
